8c63eb343b0b1c9d95de36b4b2b38826fc815f5b
Andrija Sagicc <andrija.sagic@gmail.com>
2022-09-25 17:48:43 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# Example of Mirador with plugins integration with Nuxt.js v2 
+
+This repository is designed to show different option of using IIIF standard and Mirador 3 with Nuxt.js v2.
+
+Best regards to the https://github.com/nakamura196/nuxt-mirador.
+
+## Full site
+
+https://milutinbojic.digitalna.rs/vilijam
+
+## Demo
+
+https://nakamura196.github.io/nuxt-mirador/
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Deploy on web server
+Before running generate define route in `nuxt.config.js`\
+Deploy as https://example.com just add "/"\
+Deploy in a dir as https://example.com/dir add "/dir"
+
+```
+npm run generate
+```
+Just copy /dist dir to any web server (Apache, ...)
+
+Mirador and Mirador Image Tool is licenced by Apache v2 licence.
+
+Mirador Textlayer have MIT Licence, with added Serbian translation.
---
+++ nuxt.config.js
@@ -0,0 +1,95 @@
+// const routerBase =
+//   process.env.DEPLOY_ENV === 'GH_PAGES'
+//     ? {
+//         router: {
+//           base: "/nuxt-mirador/"
+//         },
+//       }
+//     : {}
+
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  router: {
+    // base: '/iiifrep/vilijam/dist1/'
+    base: '/vilijam/'
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - Shakespeare',
+    title: 'Shakespeare',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'IIIF and Mirador integration example in Nuxt.js' },
+
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/vilijam/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+    meta: {
+      mobileApp: false,
+      mobileAppIOS: false
+    }
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {
+    apiPrefix: 'content-api',
+    dir: 'content'
+  },
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+    }
+
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  // ...routerBase,
+}
